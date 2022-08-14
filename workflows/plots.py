6f4d18f988d63e78ae4fa8a56f52ab17b78e8b9a
xin-huang <xinhuang.res@gmail.com>
2022-08-14 21:33:28 +0200

    Update based on the review

--- workflows/plots.py
+++ workflows/plots.py
@@ -47,7 +47,7 @@ archaicseeker2_2src_accuracy_mean.to_csv(snakemake.output.archaicseeker2_2src_ac
 
 methods1 = ['sstar', 'sprime', 'skovhmm']
 demography1 = ['HumanNeanderthal', 'BonoboGhost']
-samples = ['nref_10_ntgt_1', 'nref_50_ntgt_1', 'nref_10_ntgt_10', 'nref_50_ntgt_10']
+samples = ['nref_10_ntgt_1', 'nref_50_ntgt_1']
 scenarios = ['true', 'const', 'ref_tgt_only']
 accuracy1 = {
     'sstar': sstar_1src_accuracy_mean,
@@ -66,7 +66,7 @@ accuracy2 = {
     'archaicseeker2': archaicseeker2_2src_accuracy_mean,
 }
 
-fig, axs = plt.subplots(nrows=4, ncols=3, constrained_layout=True, figsize=(7.5,7.5), dpi=300)
+fig, axs = plt.subplots(nrows=2, ncols=3, constrained_layout=True, figsize=(7.5,4), dpi=350)
 gridspec = axs[0, 0].get_subplotspec().get_gridspec()
 for a in axs[:,2]:
     a.remove()
@@ -74,8 +74,6 @@ for a in axs[:,2]:
 markers = {
     'nref_10_ntgt_1': {'symbol':'.', 'size': 6},
     'nref_50_ntgt_1': {'symbol':'*', 'size': 6},
-    'nref_10_ntgt_10': {'symbol':'p', 'size': 4},
-    'nref_50_ntgt_10': {'symbol':'d', 'size': 4},
 }
 
 colors = {
@@ -98,16 +96,17 @@ titles = {
     'ChimpBonoboGhost': 'Chimpanzee-Ghost-Bonobo model',
 }
 
-auc = open(snakemake.output.auc1, 'w')
-auc.write('method\tdemography\tscenario\tsample\tAUC\n')
+zorders = {
+    'sstar': 2,
+    'skovhmm': 5,
+    'sprime': 10,
+}
 
 j = 0
 for d in demography1:
     for s in samples:
         for sc in scenarios:
             for m in methods1:
-                if (m == 'sstar') or (m == 'skovhmm'):
-                    if (s == 'nref_10_ntgt_10') or (s == 'nref_50_ntgt_10'): continue
                 if m == 'sstar': color = colors[m][sc]
                 else: color = colors[m]
                 df = accuracy1[m][
@@ -119,31 +118,42 @@ for d in demography1:
                 precision = df['precision']
                 if (m == 'sprime') or (m == 'skovhmm'):
                     if sc != 'true': continue
-                auc_score = metrics.auc(recall/100, precision/100)
-                auc.write(f'{m}\t{d}\t{sc}\t{s}\t{auc_score}\n')
-                axs[j,0].plot(recall, precision,
-                    marker=markers[s]['symbol'], ms=markers[s]['size'],
-                    c=color)
 
-    axs[j,0].set_xlabel('Recall (%)', fontsize=10)
-    axs[j,0].set_ylabel('Precision (%)', fontsize=10)
-    axs[j,0].set_xlim([-5, 105])
-    axs[j,0].set_ylim([-5, 105])
-    axs[j,0].set_title(titles[d], fontsize=8, weight='bold')
-    if j == 0: axs[j,0].text(-35, 110, 'B', fontsize=10, weight='bold')
-    if j == 1: axs[j,0].text(-35, 110, 'D', fontsize=10, weight='bold')
+                if d == 'BonoboGhost':
+                    axs[0,j].plot(recall, precision,
+                        marker=markers[s]['symbol'], ms=markers[s]['size'],
+                        c=color, zorder=zorders[m])
+                else:
+                    axs[0,j].plot(recall, precision,
+                        marker=markers[s]['symbol'], ms=markers[s]['size'],
+                        c=color)
+
+    axs[0,j].set_xlabel('Recall (%)', fontsize=10)
+    axs[0,j].set_ylabel('Precision (%)', fontsize=10)
+    axs[0,j].set_xlim([-5, 105])
+    axs[0,j].set_ylim([-5, 105])
+    axs[0,j].set_title(titles[d], fontsize=8, weight='bold')
+    if j == 0: 
+        axs[0,j].text(-35, 110, 'B', fontsize=10, weight='bold')
+        axs[0,j].plot([0,100],[2.25,2.25], c='red', alpha=0.5)
+    if j == 1: 
+        axs[0,j].text(-35, 110, 'C', fontsize=10, weight='bold')
+        axs[0,j].plot([0,100],[2,2], c='red', alpha=0.5)
+
+    f_scores = np.linspace(20, 80, num=4)
+    lines, labels = [], []
+    for f_score in f_scores:
+        x = np.linspace(1, 100)
+        y = f_score * x / (2 * x - f_score)
+        (l,) = axs[0,j].plot(x[y >= 0], y[y >= 0], color="black", alpha=0.4, linestyle='dotted', zorder=1)
+        axs[0,j].annotate("F1={0:0.0f}%".format(f_score), xy=(101, y[45] + 2), fontsize=8)
+
     j += 1
 
-auc.close()
-
-auc = open(snakemake.output.auc2, 'w')
-auc.write('method\tdemography\tsample\tsrc\tAUC\n')
-
+j = 0
 for d in demography2:
     for s in samples:
         for m in methods2:
-            if (m == 'sstar') or (m == 'skovhmm'):
-                if (s == 'nref_10_ntgt_10') or (s == 'nref_50_ntgt_10'): continue
             if m == 'sstar': color = colors[m]['true']
             else: color = colors[m]
             src1_df = accuracy2[m][
@@ -160,123 +170,36 @@ for d in demography2:
             src1_precision = src1_df['precision']
             src2_recall = src2_df['recall']
             src2_precision = src2_df['precision']
-            if m != 'archaicseeker2':
-                src1_auc_score = metrics.auc(src1_recall/100, src1_precision/100)
-                src2_auc_score = metrics.auc(src2_recall/100, src2_precision/100)
-                auc.write(f'{m}\t{d}\t{s}\tsrc1\t{src1_auc_score}\n')
-                auc.write(f'{m}\t{d}\t{s}\tsrc2\t{src2_auc_score}\n')
-            else:
-                auc.write(f'{m}\t{d}\t{s}\tsrc1\tNA\n')
-                auc.write(f'{m}\t{d}\t{s}\tsrc2\tNA\n')
-            axs[j,0].plot(src1_recall, src1_precision,
+            axs[1,j].plot(src1_recall, src1_precision,
                      marker=markers[s]['symbol'], ms=markers[s]['size'],
                      c=color, markerfacecolor='white')
-            axs[j,0].plot(src2_recall, src2_precision,
+            axs[1,j].plot(src2_recall, src2_precision,
                      marker=markers[s]['symbol'], ms=markers[s]['size'],
                      c=color, linestyle='dashdot')
 
-    axs[j,0].set_xlabel('Recall (%)', fontsize=10)
-    axs[j,0].set_ylabel('Precision (%)', fontsize=10)
-    axs[j,0].set_xlim([-5, 105])
-    axs[j,0].set_ylim([-5, 105])
-    axs[j,0].set_title(titles[d], fontsize=8, weight='bold')
-    if j == 2: axs[j,0].text(-35, 110, 'F', fontsize=10, weight='bold')
-    if j == 3: axs[j,0].text(-35, 110, 'H', fontsize=10, weight='bold')
+    axs[1,j].set_xlabel('Recall (%)', fontsize=10)
+    axs[1,j].set_ylabel('Precision (%)', fontsize=10)
+    axs[1,j].set_xlim([-5, 105])
+    axs[1,j].set_ylim([-5, 105])
+    axs[1,j].set_title(titles[d], fontsize=8, weight='bold')
+    if j == 0: 
+        axs[1,j].text(-35, 110, 'D', fontsize=10, weight='bold')
+        axs[1,j].plot([0,100],[0.2,0.2], c='red', alpha=0.5)
+        axs[1,j].plot([0,100],[4,4], c='red', linestyle='dotted')
+    if j == 1: 
+        axs[1,j].text(-35, 110, 'E', fontsize=10, weight='bold')
+        axs[1,j].plot([0,100],[2,2], c='red', alpha=0.5)
+        axs[1,j].plot([0,100],[2,2], c='red', linestyle='dotted')
+
+    f_scores = np.linspace(20, 80, num=4)
+    lines, labels = [], []
+    for f_score in f_scores:
+        x = np.linspace(1, 100)
+        y = f_score * x / (2 * x - f_score)
+        (l,) = axs[1,j].plot(x[y >= 0], y[y >= 0], color="black", alpha=0.4, linestyle='dotted', zorder=1)
+        axs[1,j].annotate("F1={0:0.0f}%".format(f_score), xy=(101, y[45] + 2), fontsize=8)
+
     j += 1
-
-auc.close()
-
-auc_1src = pd.read_csv(snakemake.output.auc1, sep="\t").dropna()
-auc_2src = pd.read_csv(snakemake.output.auc2, sep="\t").dropna()
-
-i = 0
-ytick_labels = []
-yticks = []
-for d in demography1:
-    k = 0
-    for s in samples:
-        for sc in scenarios:
-            for m in methods1:
-                if (m == 'sstar') or (m == 'skovhmm'):
-                    if (s == 'nref_10_ntgt_10') or (s == 'nref_50_ntgt_10'): continue
-                if m == 'sstar': color = colors[m][sc]
-                else: color = colors[m]
-                if (m == 'sprime') or (m == 'skovhmm'):
-                    if sc != 'true': continue
-                auc = auc_1src[
-                    (auc_1src['method'] == m) &
-                    (auc_1src['demography'] == d) &
-                    (auc_1src['scenario'] == sc) &
-                    (auc_1src['sample'] == s)
-                ]['AUC']
-                bars = axs[i,1].barh(k, auc, color=color)
-                axs[i,1].bar_label(bars, fontsize=5)
-                if s == 'nref_10_ntgt_1': ytick_labels.append('Nref=10,Ntgt=1')
-                elif s == 'nref_10_ntgt_10': ytick_labels.append('Nref=10,Ntgt=10')
-                elif s == 'nref_50_ntgt_1': ytick_labels.append('Nref=50,Ntgt=1')
-                elif s == 'nref_50_ntgt_10': ytick_labels.append('Nref=50,Ntgt=10')
-                yticks.append(k)
-                k += 1
-
-    axs[i,1].set_xlabel('AUC', fontsize=10)
-    axs[i,1].set_ylabel('Sample size', fontsize=10)
-    axs[i,1].set_yticks(yticks)
-    axs[i,1].set_yticklabels(ytick_labels, fontsize=5)
-    axs[i,1].set_title(titles[d], fontsize=8, weight='bold')
-    if i == 0: axs[i,1].text(-0.25, 12.5, 'C', fontsize=10, weight='bold')
-    if i == 1: axs[i,1].text(-0.25, 12.5, 'E', fontsize=10, weight='bold')
-    axs[i,1].set_xlim([0,1])
-    i += 1
-
-ytick_labels = []
-yticks = []
-for d in demography2:
-    k = 0
-    for s in samples:
-        for m in methods2:
-            if (m == 'sstar') or (m == 'skovhmm'):
-                if (s == 'nref_10_ntgt_10') or (s == 'nref_50_ntgt_10'): continue
-            if m == 'sstar': color = colors[m]['true']
-            else: color = colors[m]
-            if m == 'archaicseeker2': continue
-            auc_src1 = auc_2src[
-                    (auc_2src['method'] == m) &
-                    (auc_2src['demography'] == d) &
-                    (auc_2src['src'] == 'src1') &
-                    (auc_2src['sample'] == s)
-                ]['AUC']
-            auc_src2 = auc_2src[
-                    (auc_2src['method'] == m) &
-                    (auc_2src['demography'] == d) &
-                    (auc_2src['src'] == 'src2') &
-                    (auc_2src['sample'] == s)
-                ]['AUC']
-            bars = axs[i,1].barh(k, auc_src1, color=color)
-            axs[i,1].bar_label(bars, fontsize=5)
-            if s == 'nref_10_ntgt_1': ytick_labels.append('Nref=10,Ntgt=1(src1)')
-            elif s == 'nref_10_ntgt_10': ytick_labels.append('Nref=10,Ntgt=10(src1)')
-            elif s == 'nref_50_ntgt_1': ytick_labels.append('Nref=50,Ntgt=1(src1)')
-            elif s == 'nref_50_ntgt_10': ytick_labels.append('Nref=50,Ntgt=10(src1)')
-            yticks.append(k)
-            k += 1
-            bars = axs[i,1].barh(k, auc_src2, color=color)
-            axs[i,1].bar_label(bars, fontsize=5)
-            if s == 'nref_10_ntgt_1': ytick_labels.append('Nref=10,Ntgt=1(src2)')
-            elif s == 'nref_10_ntgt_10': ytick_labels.append('Nref=10,Ntgt=10(src2)')
-            elif s == 'nref_50_ntgt_1': ytick_labels.append('Nref=50,Ntgt=1(src2)')
-            elif s == 'nref_50_ntgt_10': ytick_labels.append('Nref=50,Ntgt=10(src2)')
-            yticks.append(k)
-            k += 1
-
-    axs[i,1].set_xlabel('AUC', fontsize=10)
-    axs[i,1].set_ylabel('Sample size', fontsize=10)
-    axs[i,1].set_yticks(yticks)
-    axs[i,1].set_yticklabels(ytick_labels, fontsize=5)
-    axs[i,1].set_title(titles[d], fontsize=8, weight='bold')
-    if i == 2: axs[i,1].text(-0.25, 12.5, 'G', fontsize=10, weight='bold')
-    if i == 3: axs[i,1].text(-0.25, 12.5, 'I', fontsize=10, weight='bold')
-    axs[i,1].set_xlim([0,1])
-    i += 1
 
 # legend
 subfig = fig.add_subfigure(gridspec[:,2])
@@ -286,20 +209,19 @@ sstar_line2 = plt.Line2D([0], [0], label='sstar (constant)', color=colors['sstar
 sstar_line3 = plt.Line2D([0], [0], label='sstar (only ref & tgt)', color=colors['sstar']['ref_tgt_only'])
 skovhmm_line = plt.Line2D([0], [0], label='SkovHMM', color=colors['skovhmm'])
 sprime_line = plt.Line2D([0], [0], label='SPrime', color=colors['sprime'])
-archaicseeker2_line = plt.Line2D([0], [0], label='ArchaicSeeker 2.0', color=colors['archaicseeker2'])
+archaicseeker2_line = plt.Line2D([0], [0], label='ArchaicSeeker2.0', color=colors['archaicseeker2'])
+baseline1 = plt.Line2D([0], [0], label='baseline/src1 baseline', color='red', alpha=0.5)
+baseline2 = plt.Line2D([0], [0], label='src2 baseline', color='red', linestyle='dotted')
+f1_curves = plt.Line2D([0], [0], label='iso-F1 curves', color='black', alpha=0.4, linestyle='dotted')
 nref_10_ntgt_1 = plt.Line2D([0], [0], marker=markers['nref_10_ntgt_1']['symbol'],
-                            ms=5, label='Nref=10, Ntgt=1', color='black', linewidth=0)
+                            ms=5, label='Nref = 10', color='black', linewidth=0)
 nref_50_ntgt_1 = plt.Line2D([0], [0], marker=markers['nref_50_ntgt_1']['symbol'],
-                            ms=5, label='Nref=50, Ntgt=1', color='black', linewidth=0)
-nref_10_ntgt_10 = plt.Line2D([0], [0], marker=markers['nref_10_ntgt_10']['symbol'],
-                             ms=5, label='Nref=10, Ntgt=10', color='black', linewidth=0)
-nref_50_ntgt_10 = plt.Line2D([0], [0], marker=markers['nref_50_ntgt_10']['symbol'],
-                             ms=4, label='Nref=50, Ntgt=10', color='black', linewidth=0)
+                            ms=5, label='Nref = 50', color='black', linewidth=0)
 src1 = plt.Line2D([0], [0], label='src1', color='black', marker='o', ms=4, markerfacecolor='white')
 src2 = plt.Line2D([0], [0], label='src2', color='black', marker='o', ms=4, linestyle='dotted')
 
 handles.extend([sstar_line, sstar_line2, sstar_line3, skovhmm_line, sprime_line, archaicseeker2_line,
-                nref_10_ntgt_1, nref_50_ntgt_1, nref_10_ntgt_10, nref_50_ntgt_10, src1, src2])
+                baseline1, baseline2, f1_curves, nref_10_ntgt_1, nref_50_ntgt_1, src1, src2])
 subfig.legend(handles=handles, fontsize=8, handlelength=1.5)
 
 fig.set_constrained_layout_pads(w_pad=4 / 72, h_pad=4 / 72, hspace=0, wspace=0.1)
